ac62c35e8e0d0af5ff95051b64f41ec46cd08923
Robert Nelson <robertcnelson@gmail.com>
2023-08-29 16:02:06 -0500

    onnxrt_prepare.sh: host still might need this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- dockers/dependency_build/x86/onnxrt_prepare.sh
+++ dockers/dependency_build/x86/onnxrt_prepare.sh
@@ -32,11 +32,9 @@ if [ ! -d protobuf-3.11.3  ];then
     cd ../
 fi
 
-if [ ! -f /usr/bin/cmake ] ; then
-    if [ ! -f cmake-3.22.1-linux-x86_64.sh ];then
-        wget https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-linux-x86_64.sh
-        chmod u+x cmake-3.22.1-linux-x86_64.sh 
-    fi
+if [ ! -f cmake-3.22.1-linux-x86_64.sh ];then
+    wget https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-linux-x86_64.sh
+    chmod u+x cmake-3.22.1-linux-x86_64.sh 
 fi
 
 
